33876124a37991478e7b282e35e8a96e2db7d665
vaibhavchahar <vaibhavchahar7@gmail.com>
2024-11-26 23:21:16 +0530

    It is an real estate website
    It Is

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import Header from './components/Header'
+import About from './components/About'
+import Projects from './components/Projects'
+import Testimonials from './components/Testimonials'
+import Contact from './components/Contact'
+import { ToastContainer } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+import Footer from './components/Footer'
+  
+
+const App = () => {
+  return (
+    <div>
+     <div className='w-full overflow-hidden'></div>
+     <ToastContainer/>
+      <Header/>
+      <About/>
+      <Projects/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
